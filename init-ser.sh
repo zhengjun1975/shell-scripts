55134c2a3f0e20b420985eb2ac7445fe00073587
zhengjun <10168386@qq.com>
2017-10-23 17:19:35 +0800

    server init scripts

--- init-ser.sh
+++ init-ser.sh
@@ -8,14 +8,13 @@ echo "
 
 "
 yum install -y git
-yum install -y ntp vim
 yum update -y
 systemctl disable firewalld && systemctl stop firewalld
+/usr/sbin/ntpdate 10.1.1.1
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 sed -i 's/0.centos/cn/g' /etc/ntp.conf
 sed -i '/[1,2,3].centos.pool.ntp.org iburst/d' /etc/ntp.conf
 setenforce 0
-systemctl enable ntpd && systemctl start ntpd
 timedatectl set-timezone Asia/Shanghai
 
 echo "
